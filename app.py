1134d649a5107fcd21c07e1594b055bdcc4cf68b
Mgnrega <110636199+Mgnrega@users.noreply.github.com>
2022-08-20 16:06:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,9 @@ def test2():
 def base():
     return("Hello")   
 
+from app.main import app
+import os 
 
-if __name__ == "__main__" :
-    app.run(debug= True)
+port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=port, debug=True)
