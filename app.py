0cdc8d5f3d8a7b03cd3547bab25111a7e43021d5
Mgnrega <110636199+Mgnrega@users.noreply.github.com>
2022-08-25 00:19:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import json
 import pickle
 import io
+import cv2
 import numpy as np
 from PIL import Image
 from crypt import methods
 import face_recognition
 from flask import Flask , render_template , request , redirect ,flash
 import faceDetection as fd
+import base64
+import re
+from io import BytesIO
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 classifier =pickle.loads(open('output/classifier.pickle' , 'rb').read())
 lb = pickle.loads(open('output/lable_encoder.pickle' , 'rb').read())
@@ -18,15 +24,13 @@ def test():
     persons = []
 
     if request.method=='POST':
-        img = request.files['image']
-        # print(img.file)
 
-        dataBytesIO = io.BytesIO(img.read())
-        temp = Image.open(dataBytesIO)
+        image_data = re.sub('^data:image/.+;base64,', '', request.form['imageBase64'])
+        im = Image.open(BytesIO(base64.b64decode(image_data)))
         # temp.show()
-        image = np.array(temp)
-        rgb = image
-
+        image = np.array(im)
+        # rgb = image
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         boxes = face_recognition.face_locations(rgb)
         encodings = face_recognition.face_encodings(rgb, boxes)
         y_pred = classifier.predict(encodings)
@@ -48,6 +52,7 @@ def test():
 
         # return("hh")
         result = json.dumps(persons)
+        # result.headers.add('Access-Control-Allow-Origin', '*')
         return(result)
 
 
@@ -55,11 +60,8 @@ def test():
 def faceDetection():
     persons = []
 
-#     if request.method=='POST':
-#         result = fd.recognise(request.files['image'])
-#         return(result)
-    if request.method=='GET':
-        result = fd.recognise(request.files['image'])
+    if request.method=='POST':
+        result = fd.recognise(request.form['imageBase64'])
         return(result)    
     
 @app.route("/test2" , methods=['GET' , 'POST'])
@@ -69,14 +71,6 @@ def test2():
 @app.route("/" , methods=['GET' , 'POST'])
 def base():
     return("Hello")   
-
-# @app.route("/im_size", methods=['GET' , 'POST'])
-# def process_image():
-#     file = request.files['image']
-#     # Read the image via file.stream
-#     img = Image.open(file.stream)
-
-#     return jsonify({'msg': 'success', 'size': [img.width, img.height]})
 
 if __name__ == "__main__":
         app.run(debug=True)
